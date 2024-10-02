63039d6d76e44ea5ce45221fe33f732a1cb7b5a7
Nicolas-Kochhann <nicolas.kochhann@aluno.feliz.ifrs.edu.br>
2024-10-02 12:10:14 -0300

    2/10
    Fiz o commit e alterei um pouco a query.

--- buscaProjeto.php
+++ buscaProjeto.php
@@ -1,9 +1,11 @@
 <?php
     require_once("config.php");
 
+    //
+
     if(isset($_POST['botaoBuscar'])) {
         $entrada_busca = $_POST['titulo'];
-        $sql = "SELECT projetos.titulo, temas.descricao, disciplinas.descricao
+        $sql = "SELECT projetos.titulo, GROUP_CONCAT(temas.id_tema, temas.descricao SEPARATOR ', ') AS temas, GROUP_CONCAT(disciplinas.descricao SEPARATOR ', ') AS disciplinas
                 FROM projetos
                 JOIN projetos_temas ON projetos.id_projeto = projetos_temas.id_projeto
                 JOIN temas ON projetos_temas.id_tema = temas.id_tema
@@ -13,14 +15,14 @@
 
         echo $sql;
 
-        die;
+        //die;
 
         $result = $db->query($sql);
 
         if ($result->num_rows > 0){
             $saida = "<ul>";
             while ($row = $result->fetch_assoc()){
-                $saida .= "<li>" .$row['titulo']. " - " .$row['tema.descricao']. "<br>" . $row['disciplina.descricao']. "</li>";      
+                $saida .= "<li>" .$row['titulo']. " - " .$row['temas']. "<br>" . $row['disciplinas']. "</li>";      
             }
 
             $saida .= "</ul>";
