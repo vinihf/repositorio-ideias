2f9e0fdb6618674b3ed01be73b01bb08c1297f97
Nicolas-Kochhann <nicolas.kochhann@aluno.feliz.ifrs.edu.br>
2024-10-16 09:16:47 -0300

    16/10

--- busca.php
+++ busca.php
@@ -8,20 +8,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Buscador</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Busque sua ideia ABP aqui!</h1>
+    <div class="row">
+        <div class="col"></div>
+        <div class="col">
+            <h1>Busque sua ideia ABP aqui!</h1>
+        </div>
+        <div class="col"></div>
+    </div>
     <br></br>
 
-    <form action="buscaProjeto.php" method="post">
-        <input type="text" name="titulo" placeholder="Digite o título a pesquisar...">
-        <button name="botaoBuscar" id="buscar" value="submit">Buscar</button>
-    </form>
+    <div class=row>
+        <div class="col"></div>
+        <div class="col">
+            <form action="buscaProjeto.php" method="post">
+                <input type="text" name="titulo" placeholder="Digite o título a pesquisar...">
+                <button name="botaoBuscar" id="buscar" value="submit">Buscar</button>
+            </form>
+        </div>
+        <div class="col"></div>
+    </div>
+
+    <br></br>
+    
 
     <?php if (isset($_GET['resultado'])): ?>
-        <h2>Resultados da busca:</h2>
-        <div><?php echo $_GET['resultado']; ?></div>
-    <?php endif; ?>
+       
+        <div class="row">
+            <div class="col"></div>
+            <div class="col"> <h2>Resultados da busca:</h2>
+                <?php echo $_GET['resultado']; ?>
+            </div>
+            <div class="col"></div>
+        <?php endif; ?>
+        </div>
 
 </body>
 </html>
--- cria.php
+++ cria.php
@@ -22,6 +22,9 @@ $artefatos = $db->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Criador</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
 </head>
 <body>
     <h1>Vamos criar sua idéia de ABP?</h1>
@@ -93,7 +96,6 @@ $artefatos = $db->query($sql);
             <textarea name="referencias" id="referencias" cols="50" rows="9"></textarea>
 
         <br>
-
         <button name="botao" id="submit" value="publicar">Publicar ideia</button>
     </form>
 
